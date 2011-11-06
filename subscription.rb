0f1574931fca51cb479156bcf2566716203a28ed
Sebastian Waisbrot <seppo0010@gmail.com>
2011-11-05 23:53:05 -0300

    Fix require adding rubygems

--- subscription.rb
+++ subscription.rb
@@ -25,6 +25,7 @@
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied, of Salvatore Sanfilippo.
 
+require 'rubygems'
 require 'uri'
 require 'net/http'
 require 'simple-rss'
